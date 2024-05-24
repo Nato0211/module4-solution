4938960910f8e8db83891654381e1aa95c02bd53
Renato Reyes Valenzuela <u20221b471@upc.edu.pe>
2024-05-24 14:25:08 -0500

    js

---
+++ js/Funcion.js
@@ -0,0 +1,34 @@
+var nombres = [
+    "Ana",
+    "Luis",
+    "María",
+    "Carlos",
+    "Elena",
+    "Pedro",
+    "Lucía",
+    "Jorge",
+    "Laura",
+    "Raúl",
+    "Clara",
+    "Fernando",
+    "Sofía",
+    "Pablo",
+    "Isabel",
+    "Ricardo",
+    "Marta",
+    "Diego",
+    "Patricia",
+    "Javier"
+];
+
+var i = 0; // Inicializar la variable i
+
+while(i < nombres.length) {
+    var name = nombres[i];
+    if(name[0] === 'J' || name[0] === 'j') {
+        console.log("GoodBye J" + name);
+    }else{
+        console.log("Hello " + name);
+    }
+    i++; // Incrementar i en cada iteración para evitar un bucle infinito
+}
